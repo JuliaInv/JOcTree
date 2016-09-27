1f629ce721caafba2f0e8490aaa77557ffb00a00
Christoph Schwarzbach <cschwarz@eos.ub.ca>
2016-09-27 16:39:03 -0700

    Add optional parameter to createOcTreeFromBox and tighten bounding box
    This commit contains three changes to the function createOcTreeFromBox.
    
     - A new input parameter bsz has been added. The OcTree is generated
       starting with fine cells of block size bsz. The new input parameter
    	 is optional. Its default value bsz = 1 ensures backwards
    	 compatibility.
    
     - The smallest permissible value for input parameter nf has changed
       from 1 to 0.
    
     - Fine cells are placed tighter over the given bounding box xa, xb,
       ya, yb, za, zb. Together with the setting nf = 0 it is now possible
    	 to create an OcTree with fine cells only inside the given bounding
    	 box if the limits coincide with grid lines of the next coarser level.

--- src/createOcTreeFromBox.jl
+++ src/createOcTreeFromBox.jl
@@ -7,7 +7,8 @@ function createOcTreeFromBox(
   xa::AbstractFloat, xb::AbstractFloat,
   ya::AbstractFloat, yb::AbstractFloat,
   za::AbstractFloat, zb::AbstractFloat,
-  nf::Array{Int,1}, nc::Array{Int,1})
+  nf::Array{Int,1}, nc::Array{Int,1}, 
+  bsz::Int = 1)
 # S = createOcTreeFromBox( ...
 #   x0, y0, z0, nx, ny, nz, hx, hy, hz, xa, xb, ya, yb, za, zb, nf, nc)
 #
@@ -37,15 +38,22 @@ if nextpow2(nx) != nx || nextpow2(ny) != ny || nextpow2(nz) != nz
   error("nx, ny, nz must be powers of 2.")
 end
 
-if !((length(nf) == 6) && all(nf .> 0))
-  error("nf must be an all positive vector of length 6")
+if !((length(nf) == 6) && all(nf .>= 0))
+  error("nf must be an all non-negative vector of length 6")
 end
 if !((length(nc) == 6) && all(nc .> 0))
   error("nc must be an all positive vector of length 6")
 end
 
+if bsz < 1 || nextpow2(bsz) != bsz
+	error("bsz must be greater or equal 1 and a power of 2")
+end
+
 # maximum number of cell sizes
-nbsz = minimum(round(Int64,log2([nx, ny, nz])))
+nbsz = minimum(round(Int64,log2([nx, ny, nz]))) - round(Int64,log2(bsz))
+if nbsz < 1
+  error("bsz is too large")
+end
 
 # initialize (k,j,i,bsz) array
 S = Array(Int, (0,4))
@@ -58,31 +66,50 @@ yb = (yb - y0) / hy + 1.0
 za = (za - z0) / hz + 1.0
 zb = (zb - z0) / hz + 1.0
 
-# start with fine cells around box
-i1 = floor(Integer,xa) 
-i2 = floor(Integer,xb)
-j1 = floor(Integer,ya)
-j2 = floor(Integer,yb)
-k1 = floor(Integer,za)
-k2 = floor(Integer,zb)
-if xa - i1 < 0.5
-  i1 = i1 - 1
+# start with fine cells around box, allow for small tolerance
+i1 =     floor(Integer,xa) 
+i2 = min(floor(Integer,xb), nx)
+j1 =     floor(Integer,ya)
+j2 = min(floor(Integer,yb), ny)
+k1 =     floor(Integer,za)
+k2 = min(floor(Integer,zb), nz)
+t2 = sqrt(eps())
+t1 = 1.0 - t2
+if xa - i1 > t1 * i1
+  i1 = i1 + 1
 end
-if xb - i2 > 0.5
-  i2 = i2 + 1
+if xb - i2 < t2 * i2
+  i2 = i2 - 1
 end
-if ya - j1 < 0.5
-  j1 = j1 - 1
+if ya - j1 > t1 * j1
+  j1 = j1 + 1
 end
-if yb - j2 > 0.5
-  j2 = j2 + 1
+if yb - j2 < t2 * j2
+  j2 = j2 - 1
 end
-if za - k1 < 0.5
-  k1 = k1 - 1
+if za - k1 > t1 * k1
+  k1 = k1 + 1
 end
-if zb - k2 > 0.5
-  k2 = k2 + 1
+if zb - k2 < t2 * k2
+  k2 = k2 - 1
 end
+if i1 > i2
+	i1,i2 = i2,i1
+end
+if j1 > j2
+	j1,j2 = j2,j1
+end
+if k1 > k2
+	k1,k2 = k2,k1
+end
+
+# make sure that indices live on grid with block size bsz
+i1 -= mod(i1 - 1, bsz)
+i2 -= mod(i2 - 1, bsz)
+j1 -= mod(j1 - 1, bsz)
+j2 -= mod(j2 - 1, bsz)
+k1 -= mod(k1 - 1, bsz)
+k2 -= mod(k2 - 1, bsz)
 
 # initial number of cells per layer
 nl = nf
@@ -91,7 +118,7 @@ nl = nf
 for m = 1:nbsz
   
   # block size
-  b = 2^(m-1)
+  b = bsz * 2^(m-1)
   
   # pad
   if m < nbsz
@@ -136,9 +163,9 @@ for m = 1:nbsz
   if m > 1
     
     # mask for inner region with finer cells, coordinate form
-    u = [trues(round(Int64,(i1 - i3) / b)); falses(round(Int64,(i2 - i1) / b + 1)); trues(round(Int64,(i4 - i2) / b));]
-    v = [trues(round(Int64,(j1 - j3) / b)); falses(round(Int64,(j2 - j1) / b + 1)); trues(round(Int64,(j4 - j2) / b));]
-    w = [trues(round(Int64,(k1 - k3) / b)); falses(round(Int64,(k2 - k1) / b + 1)); trues(round(Int64,(k4 - k2) / b));]
+    u = [trues(round(Int64, (i1 - i3) / b)); falses(round(Int64, (i2 - i1) / b + 1)); trues(round(Int64, (i4 - i2) / b));]
+    v = [trues(round(Int64, (j1 - j3) / b)); falses(round(Int64, (j2 - j1) / b + 1)); trues(round(Int64, (j4 - j2) / b));]
+    w = [trues(round(Int64, (k1 - k3) / b)); falses(round(Int64, (k2 - k1) / b + 1)); trues(round(Int64, (k4 - k2) / b));]
 
     # we are done if mask is all false
     if !(any(u) || any(v) || any(w))
@@ -154,7 +181,7 @@ for m = 1:nbsz
   else
     
     # at the first, finest level, use all cells
-    q = trues(i4 - i3 + 1, j4 - j3 + 1, k4 - k3 + 1)
+    q = trues(round(Int64, (i4 - i3) / b + 1), round(Int64, (j4 - j3) / b + 1), round(Int64, (k4 - k3) / b + 1))
     
     # change to default number of cells
     nl = nc
@@ -163,10 +190,10 @@ for m = 1:nbsz
   
   # index vectors for current cell size
   i,j,k = ndgrid([i3:b:i4;], [j3:b:j4;], [k3:b:k4;])
-  bsz = fill(b, size(i))
+  s = fill(b, size(i))
   
   # add only those cells that pad the inner, finer core region using mask
-  S = [S; i[q] j[q] k[q] bsz[q];]
+  S = [S; i[q] j[q] k[q] s[q];]
   
   # core region for next cell size, shift upper limit by current cell size
   # to position of next larger cell size
@@ -202,11 +229,13 @@ createOcTreeFromBox(
   ya::AbstractFloat, yb::AbstractFloat,
   za::AbstractFloat, zb::AbstractFloat,
   nf::Int,
-  nc::Int) =
+  nc::Int,
+  bsz::Int = 1) =
 createOcTreeFromBox(
   x0, y0, z0,
   nx, ny, nz, 
   hx, hy, hz,
   xa, xb, ya, yb, za, zb,
   [nf; nf; nf; nf; nf; nf;],
-  [nc; nc; nc; nc; nc; nc;])
+  [nc; nc; nc; nc; nc; nc;],
+  bsz)
