a40d57555f8485e88a537e22bcd6719e1c5e0dbb
Patrick Belliveau <patrick.t.belliveau@gmail.com>
2017-04-21 23:27:52 -0700

    Added getdFaceMassMatrix (got by checking out getFaceMassMatrix.jl from Christoph's fork of JOcTree. Added tests of edge and face mass matrix derivatives.

--- src/getFaceMassMatrix.jl
+++ src/getFaceMassMatrix.jl
@@ -1,43 +1,65 @@
-export getFaceMassMatrix #, getdFaceMassMatrix (derivative not yet implemented 
-                                               #for nodal interpolation approach to mass matrices
-
-
+export getFaceMassMatrix, getdFaceMassMatrix
 
 function getFaceMassMatrix(M::OcTreeMeshFV,sigma::Vector)
-    if isempty(M.Pf)
-        M.Pf = getFaceMassMatrixIntegrationMatrix(M.S,M.h)
-    end
-    P = M.Pf    
-    n = length(sigma)
-    if n == M.nc
-        M = P' * kron(speye(24),spdiagm(sigma)) * P
-    elseif n == 3 * M.nc
-        M = P' * kron(speye(8),spdiagm(sigma)) * P
-    elseif n == 6 * M.nc
-        S11 = spdiagm(sigma[:,1])
-        S22 = spdiagm(sigma[:,2])
-        S33 = spdiagm(sigma[:,3])
-        S12 = spdiagm(sigma[:,4])
-        S13 = spdiagm(sigma[:,5])
-        S23 = spdiagm(sigma[:,6])    
-        Sig = [S11 S12 S13;
-               S12 S22 S23;
-               S13 S23 S33]
-        M = P'* kron(speye(8),Sig) * P
-    else
-        error("Invalid size of sigma")
-    end
-    return M
+  # For octree meshes
+  if isempty(M.Pf)
+    M.Pf = getFaceMassMatrixIntegrationMatrix(M.S, M.h)
+  end
+  P = M.Pf
+  n = length(sigma)
+  if n == M.nc
+    M = P' * kron(speye(24),spdiagm(sigma)) * P
+  elseif n == 3 * M.nc
+    M = P' * kron(speye(8),spdiagm(sigma)) * P
+  elseif n == 6 * M.nc
+    R12 = kron(sparse([2,1,3],[1,2,3],1),speye(Int,M.nc))
+    R23 = kron(sparse([1,3,2],[1,2,3],1),speye(Int,M.nc))
+    D   = spdiagm(sigma[       1:3*M.nc])
+    N   = spdiagm(sigma[3*M.nc+1:6*M.nc])
+    S   = D + R12 * N * R23 + R23 * N * R12
+    M   = P' * kron(speye(8),S) * P
+   else
+     error("Invalid size")
+   end
+	 return M
 end
 
+# For backwards compatibility
+getdFaceMassMatrix(M::OcTreeMeshFV,v::Vector) = getdFaceMassMatrix(M,ones(M.nc),v)
+
+function getdFaceMassMatrix(M::OcTreeMeshFV,sigma::Vector,v::Vector)
+  # Derivative
+  if isempty(M.Pf)
+    M.Pf = getFaceMassMatrixIntegrationMatrix(M.S, M.h)
+  end
+  P = M.Pf
+  w = P * v
+  n = length(sigma)
+  if n == M.nc
+    dM = P' * spdiagm(w) * kron(ones(24),speye(M.nc))
+  elseif n == 3 * M.nc
+    dM = P' * spdiagm(w) * kron(ones(8),speye(3*M.nc))
+  elseif n == 6 * M.nc
+    R12 = kron(speye(Int,8),kron(sparse([2,1,3],[1,2,3],1),speye(Int,M.nc)))
+    R23 = kron(speye(Int,8),kron(sparse([1,3,2],[1,2,3],1),speye(Int,M.nc)))
+    D   = spdiagm(w)
+    N   = R12 * spdiagm(R23 * w) + R23 * spdiagm(R12 * w)
+    dM  = hcat(
+      P' * D * kron(ones(8),speye(3*M.nc)),
+      P' * N * kron(ones(8),speye(3*M.nc)))
+  else
+    error("Invalid size")
+  end
+	return dM
+end
+
+
 function getFaceMassMatrixIntegrationMatrix(S::SparseArray3D,h)
-    #  M = getFaceMassMatrix(S,h,sigma)
-    #
     
-    n = S.sz;
-    nex = n + [1, 0, 0]
-    ney = n + [0, 1, 0]
-    nez = n + [0, 0, 1]
+    n   = S.sz;
+    nex = (n[1]+1,n[2],n[3])
+    ney = (n[1],n[2]+1,n[3])
+    nez = (n[1],n[2],n[3]+1)
     
     i,j,k,bsz = find3(S)
     ex,ey,ez = getFaceNumbering(S)
@@ -47,47 +69,43 @@ function getFaceMassMatrixIntegrationMatrix(S::SparseArray3D,h)
     nz = nnz(ez)
     nc = nnz(S)
     c  = collect(1:nc)
-    uc = sqrt(bsz.^3 * prod(h)/8)
+    uc = sqrt(bsz.^3 * prod(h) / 8)
     
-    Px1 = ex.SV[sub2ind(nex,i,j,k),1]
-    Py1 = ey.SV[sub2ind(ney,i,j,k),1]
-    Pz1 = ez.SV[sub2ind(nez,i,j,k),1]
+    Px1 = getNodesFromIndices(ex.SV,nex,i,j,k)
+    Py1 = getNodesFromIndices(ey.SV,ney,i,j,k)
+    Pz1 = getNodesFromIndices(ez.SV,nez,i,j,k)
     
-    Px2 = ex.SV[sub2ind(nex,i+bsz,j,k),1]
-    Py2 = ey.SV[sub2ind(ney,i,j,k),1]
-    Pz2 = ez.SV[sub2ind(nez,i,j,k),1]
+    Px2 = getNodesFromIndices(ex.SV,nex,i+bsz,j,k)
+    Py2 = getNodesFromIndices(ey.SV,ney,i,j,k)
+    Pz2 = getNodesFromIndices(ez.SV,nez,i,j,k)
     
-    Px3 = ex.SV[sub2ind(nex,i,j,k),1]
-    Py3 = ey.SV[sub2ind(ney,i,j+bsz,k),1]
-    Pz3 = ez.SV[sub2ind(nez,i,j,k),1]
+    Px3 = getNodesFromIndices(ex.SV,nex,i,j,k)
+    Py3 = getNodesFromIndices(ey.SV,ney,i,j+bsz,k)
+    Pz3 = getNodesFromIndices(ez.SV,nez,i,j,k)
     
-    Px4 = ex.SV[sub2ind(nex,i+bsz,j,k),1]
-    Py4 = ey.SV[sub2ind(ney,i,j+bsz,k),1]
-    Pz4 = ez.SV[sub2ind(nez,i,j,k),1]
+    Px4 = getNodesFromIndices(ex.SV,nex,i+bsz,j,k)
+    Py4 = getNodesFromIndices(ey.SV,ney,i,j+bsz,k)
+    Pz4 = getNodesFromIndices(ez.SV,nez,i,j,k)
     
-    Px5 = ex.SV[sub2ind(nex,i,j,k),1]
-    Py5 = ey.SV[sub2ind(ney,i,j,k),1]
-    Pz5 = ez.SV[sub2ind(nez,i,j,k+bsz),1]
+    Px5 = getNodesFromIndices(ex.SV,nex,i,j,k)
+    Py5 = getNodesFromIndices(ey.SV,ney,i,j,k)
+    Pz5 = getNodesFromIndices(ez.SV,nez,i,j,k+bsz)
     
-    Px6 = ex.SV[sub2ind(nex,i+bsz,j,k),1]
-    Py6 = ey.SV[sub2ind(ney,i,j,k),1]
-    Pz6 = ez.SV[sub2ind(nez,i,j,k+bsz),1]
+    Px6 = getNodesFromIndices(ex.SV,nex,i+bsz,j,k)
+    Py6 = getNodesFromIndices(ey.SV,ney,i,j,k)
+    Pz6 = getNodesFromIndices(ez.SV,nez,i,j,k+bsz)
     
-    Px7 = ex.SV[sub2ind(nex,i,j,k),1]
-    Py7 = ey.SV[sub2ind(ney,i,j+bsz,k),1]
-    Pz7 = ez.SV[sub2ind(nez,i,j,k+bsz),1]
+    Px7 = getNodesFromIndices(ex.SV,nex,i,j,k)
+    Py7 = getNodesFromIndices(ey.SV,ney,i,j+bsz,k)
+    Pz7 = getNodesFromIndices(ez.SV,nez,i,j,k+bsz)
     
-    Px8 = ex.SV[sub2ind(nex,i+bsz,j,k),1]
-    Py8 = ey.SV[sub2ind(ney,i,j+bsz,k),1]
-    Pz8 = ez.SV[sub2ind(nez,i,j,k+bsz),1]
+    Px8 = getNodesFromIndices(ex.SV,nex,i+bsz,j,k)
+    Py8 = getNodesFromIndices(ey.SV,ney,i,j+bsz,k)
+    Pz8 = getNodesFromIndices(ez.SV,nez,i,j,k+bsz)
     
-    
-    sp1(Q) = sparse(1:Base.nnz(Q),Q.nzval,uc,nc,nx)
-    sp2(Q) = sparse(1:Base.nnz(Q),Q.nzval,uc,nc,ny)
-    sp3(Q) = sparse(1:Base.nnz(Q),Q.nzval,uc,nc,nz)
-    # sp1(Q) = sparse(1:Base.nnz(Q),Base.nonzeros(Q),ones(Base.nnz(Q)),nc,nx)
-    # sp2(Q) = sparse(1:Base.nnz(Q),Base.nonzeros(Q),ones(Base.nnz(Q)),nc,ny)
-    # sp3(Q) = sparse(1:Base.nnz(Q),Base.nonzeros(Q),ones(Base.nnz(Q)),nc,nz)
+    sp1(j) = sparse(c,j,uc,nc,nx)
+    sp2(j) = sparse(c,j,uc,nc,ny)
+    sp3(j) = sparse(c,j,uc,nc,nz)
     
     P1 = blkdiag(sp1(Px1),sp2(Py1),sp3(Pz1))
     P2 = blkdiag(sp1(Px2),sp2(Py2),sp3(Pz2))
@@ -98,20 +116,19 @@ function getFaceMassMatrixIntegrationMatrix(S::SparseArray3D,h)
     P7 = blkdiag(sp1(Px7),sp2(Py7),sp3(Pz7))
     P8 = blkdiag(sp1(Px8),sp2(Py8),sp3(Pz8))
     
-    #v     = prod(h) * (bsz.^3)
-    # Sigma = [spdiagm(sigma[:,1].*v)  spdiagm(sigma[:,4].*v)  spdiagm(sigma[:,5].*v);
-    #          spdiagm(sigma[:,4].*v)  spdiagm(sigma[:,2].*v)  spdiagm(sigma[:,6].*v);
-    #          spdiagm(sigma[:,5].*v)  spdiagm(sigma[:,6].*v)  spdiagm(sigma[:,3].*v)]
-    # 
-    # 
-    # Massf = 0.125*(P1'*Sigma*P1 + P2'*Sigma*P2 + P3'*Sigma*P3 + P4'*Sigma*P4 +
-    #                P5'*Sigma*P5 + P6'*Sigma*P6 + P7'*Sigma*P7 + P8'*Sigma*P8);
+    P = vcat(P1, P2, P3, P4, P5, P6, P7, P8)
     
-    #P = [P1;P2;P3;P4;P5;P6;P7;P8]
-    # Following line faster than line above because hcat + transpose is
-    # faster than vcat.
-    P = [P1' P2' P3' P4' P5' P6' P7' P8']'
     return P
     
-end  # function getFaceMassMatrix
+end
 
+function getNodesFromIndices(sv,mm,i0::Vector{Int},j0::Vector{Int},k0::Vector{Int})
+	
+	jj = sub2ind(mm,i0,j0,k0)
+  v  = Array(Int64, length(jj))
+  for i = 1:length(jj)
+    v[i] = sv[jj[i]]
+  end
+	return v
+	
+end
